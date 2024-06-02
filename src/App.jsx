770661f21514ad825fea389bdd0ddf54c89e89cf
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-06-02 15:55:22 -0700

    finalize markdown editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setmarkdown] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='container'>
+      <div className="left-side">
+        <textarea onChange={(e) => { setmarkdown(e.target.value) }} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="right-side">
+        <a href="https://www.markdownguide.org/cheat-sheet/" target='_blank' className='cheatsheet-link'> Click to See markdown Cheatsheet </a>
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
